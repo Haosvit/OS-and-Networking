808af7d0b336046e528db56b8fdc6f5e3392d60e
Hao Svit <haonguyen.svit@gmail.com>
2016-06-01 12:15:34 +0700

    * fixed log converter

--- src/com/hao/keylogger/utils/LogConverter.java
+++ src/com/hao/keylogger/utils/LogConverter.java
@@ -51,8 +51,7 @@ public class LogConverter {
 			if (c == '[') {
 				if (log.charAt(i + 1) == ' ') {
 					sb.append(c);
-				}
-				else {
+				} else {
 					specialKey += c;
 					while (true) {
 						i++;
@@ -93,6 +92,46 @@ public class LogConverter {
 						c = Character.toUpperCase(c);
 					}
 					if (isShift) {
+						if (Character.isDigit(c)) {
+
+							switch (c) {
+							case '1':
+								c = '!';
+								break;
+							case '2':
+								c = '@';
+								break;
+							case '3':
+								c = '#';
+								break;
+							case '4':
+								c = '$';
+								break;
+							case '5':
+								c = '%';
+								break;
+							case '6':
+								c = '^';
+								break;
+							case '7':
+								c = '&';
+								break;
+							case '8':
+								c = '*';
+								break;
+							case '9':
+								c = '(';
+								break;
+							case '0':
+								c = ')';
+								break;
+							}
+						} else if (c == '-') {
+							c = '_';
+						} else if (c == '=') {
+							c = '+';
+						}
+						
 						if (Character.isUpperCase(c)) {
 							c = Character.toLowerCase(c);
 						} else {
@@ -107,11 +146,5 @@ public class LogConverter {
 		}
 
 		return sb.toString();
-	}
-
-	public static void main(String[] args) {
-		String log = "R O I [Space] N G U O I [Space] T A [Space] D E N [CAP_ON] [Space] T H O E [Backspace] [Backspace] E O [Space] H O [Space] H A N G [Space] D O I [Space] B N [Backspace] E N [Enter] M O T [Space] [CAP_OFF] N G A Y [Space] N E N [Space] [Left Shift] D U Y E N [Space] M O T [Space] [Left Shift] B U O C [Space] E M [Space] [Left Shift] E N [Backspace] [Backspace] [Left Shift] N E N [Space] V O [Space] [Left Shift] H I E N [Enter] [Left Control] [Left Control] A C [Left Shift] [Left Shift] [Left Shift] [End] [Left Shift] [Left] [Left] [Left Control] V [Left Control] [F11] [Left Windows] R [Enter] C A I [Space] B E E P [Space] 1 2 3 4 ` [F1] [F2] [Space] [Right Control] [Right Control] A [Right] [Num 0] [Num 1] [Num 0] [Num 2] [Num Del] [Num 2] 2 0 [Space] 3 0 [Space] [ [Left Shift] L [Backspace] [Left Shift] B A C K S P A C E ] [Space] [ B A C [Backspace] [Backspace] [Backspace] [Backspace] [ E N T E R N [Backspace] [Backspace] [Backspace] [Backspace] [Backspace] [Backspace] [Backspace] [ [Left Shift] E N T E R ] [Space] [Left Shift] [ [Left Shift] E N T E R ] ";
-		LogConverter lc = new LogConverter();
-		System.out.println(lc.convert(log));
 	}
 }
